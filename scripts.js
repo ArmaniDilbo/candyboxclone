6628fa1605f85671671d43dde5925d6fcf8008bd
ArmaniDilbo <sp.tepper@gmail.com>
2025-02-28 21:15:01 -0500

    added candies lollipops + weapons button

--- scripts.js
+++ scripts.js
@@ -1,14 +1,18 @@
 let candiesDisplayEm = document.getElementById("candies");
 let lollipopsDisplayEm = document.getElementById("lollipops");
-let weaponRackDisplayEm = document.getElementById("weapons-rack")
+let weaponRackDisplayEm = document.getElementById("weapons")
 let candies = 0;
 let lollipops = 0;
 
+weaponRackDisplayEm.style.display = "none";
 function addCandies() {
     candies += 1;
     candiesDisplayEm.textContent = candies;
     if (candies < 100) {
         weaponRackDisplayEm.style.display = "none";
+    }
+    else{
+        weaponRackDisplayEm.style.display = "block"
     }
 }
 
@@ -17,7 +21,6 @@ function addLollipops() {
     lollipopsDisplayEm.textContent = lollipops
 
 }
-
 
 setInterval(addCandies,1000)
 setInterval(addLollipops,10000)
